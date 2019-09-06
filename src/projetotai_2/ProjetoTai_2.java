84c82e5b5fdabe2b577421882c7f581cf39756e4
fasilva87 <m.fabriciosilva@gmail.com>
2019-09-05 22:34:04 -0300

    atualizado2

--- src/projetotai_2/ProjetoTai_2.java
+++ src/projetotai_2/ProjetoTai_2.java
@@ -7,6 +7,8 @@ public class ProjetoTai_2 {
    
     public static void main(String[] args) {
         System.out.println("Olá 4SI");
+        System.out.println("Teste");
+        System.out.println("123");
     }
     
 }
