6dfd99d9406c19bfaf1783c4667232e01500db20
fasilva87 <m.fabriciosilva@gmail.com>
2019-09-05 22:39:32 -0300

    atualizado3

--- src/projetotai_2/ProjetoTai_2.java
+++ src/projetotai_2/ProjetoTai_2.java
@@ -1,14 +1,32 @@
 
 package projetotai_2;
 
+import java.util.Scanner;
+
 
 public class ProjetoTai_2 {
 
    
     public static void main(String[] args) {
-        System.out.println("Olá 4SI");
-        System.out.println("Teste");
-        System.out.println("123");
+         double watts, horas;
+        double consumo, valor;
+        int dias;
+        Scanner scan = new Scanner (System.in);
+        
+        System.out.println("Digite o consumo em watts: ");
+        watts = scan.nextDouble();
+        
+        System.out.println("Digite a quantidade de horas de uso: ");
+        horas = scan.nextDouble();
+        
+        System.out.println("Digite a quantidade de dias: ");
+        dias = scan.nextInt();
+        
+        consumo =(watts*horas*dias) / 1000; 
+        valor = (consumo) * 0.69;
+        System.out.println("O consumo total é de : " + consumo + " Watts/dia(s)" );
+        
+        System.out.printf("O valor total é R$%.2f " , valor);
     }
     
 }
